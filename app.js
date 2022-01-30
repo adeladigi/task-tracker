a742a2d579e872482bc28d663adc470ee7b7c38d
Marcos <greendogpop@gmail.com>
2022-01-30 10:58:24 -0500

    added more stuff to client  profile

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 // Dependencies
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const md5 = require("md5");
+const res = require('express/lib/response');
 
 // settings
 app.set("view engine", "ejs");
@@ -12,22 +15,89 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/timeDB", {useNewUrlparser: true});
 
+const adminSchema = new mongoose.Schema ({
+    email: String,
+    password: String
+});
+
+const secret = process.env.SECRET;
+
+
+
+
+
+const Admin = new mongoose.model("Admin",  adminSchema);
+
 
 // App.Get
 app.get("/", function(req, res){
-    res.render("landingPage");
+    res.render("loginPage");
 });
 
 app.get("/profile", function(req, res){
     res.render("profilePage");
-})
+});
+
+app.get("/register", function(req, res){
+     res.render("registerPage");
+});
+
+app.get("/client", function(req, res){
+    res.render("clientTemplate");
+});
+
 
 // App.Post
-app.post("/auth", function(req, res){
+app.post("/register", function(req, res){
    const email = req.body.email;
+   const username = req.body.username;
+   const password = md5(req.body.password);
+
+    const newAdmin = Admin({
+    email: email,
+    password: password
+   });
+
+    newAdmin.save(function(err){
+       if(err){
+           console.log(err)
+       }else{
+        res.redirect("/");
+       }
+
+   });
    
-   console.log(email);
-   res.redirect("/profile");
+//    console.log("email is :"+email);
+//    console.log("email is :"+password);
+});
+
+
+app.post("/login", function(req, res){
+    const emailTyped = req.body.emailTyped;
+    const passwordTyped = md5(req.body.passwordTyped);
+
+    Admin.findOne({email: emailTyped}, function(err, foundAdmin){
+        if(err){
+            console.log("ERROR: 304");
+            
+        }else{
+            // If email matches
+            if(foundAdmin){
+                // check if password matches
+                if(foundAdmin.password === passwordTyped){
+                    res.redirect("/profile");
+                }else{
+                    console.log("Wrong password");
+                    res.redirect("/");
+                }
+            }
+           
+        
+        }
+    });
+    
+    // console.log("This is the email: "+usernameTyped);
+    // console.log("This is the password: "+passwordTyped);
 });
 
 
@@ -35,3 +105,12 @@ app.post("/auth", function(req, res){
 app.listen(3000, function(req, res){
     console.log("Server started on port 3000!");
 });
+
+
+
+
+//1# continue to higher level encryption.
+
+//2# create client template page and display the data that client will need.
+
+//3# See if you can create a option for light mode and dark mode.
