0eb6261eb545ddce170bbe1949577016debf5b4c
Marcos <greendogpop@gmail.com>
2022-01-31 16:02:54 -0500

    added functions that submit info for save btn and add task btn , beacuse this works better then forms submit bottons

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const res = require('express/lib/response');
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.use(bodyParser.json());
 
 mongoose.connect("mongodb://localhost:27017/timeDB", {useNewUrlparser: true});
 
@@ -24,8 +25,12 @@ const secret = process.env.SECRET;
 
 const Admin = new mongoose.model("Admin",  adminSchema);
 
+//Task switch
+let taskLight = false;
 
-const taskList = ["Straight Leg Raises", "Lateral Leg Raises", "Back Lunges"];
+
+// Task Arry
+const taskList = [];
 
 // App.Get
 app.get("/", function(req, res){
@@ -45,7 +50,11 @@ app.get("/client", function(req, res){
 });
 
 app.get("/edit", function(req, res){
-   res.render("editPage", {taskList: taskList });
+
+   res.render("editPage", {
+       taskList: taskList,
+       taskLight: taskLight 
+    });
 });
 
 
@@ -104,8 +113,8 @@ app.post("/login", function(req, res){
 });
 
 app.post("/client-bio", function(req, res){
-    const firstName = req.body.firstName;
-    const lastName = req.body.lastName;
+    const firstName = req.body.fname;
+    const lastName = req.body.lname;
     const gender = req.body.gender;
     const address = req.body.address;
     const state = req.body.state;
@@ -114,13 +123,63 @@ app.post("/client-bio", function(req, res){
     const data = req.body;
 
     console.log(data);
+
+    //must create function where task gets added
+    //to database
+
+    for(let i = 0; i < taskList.length + 2; i++){
+        taskList.pop();
+    }
+
+
+    taskLight = false;
     
     res.redirect("/edit");
 });
 
 app.post("/add-task", function(req, res){
-   console.log(req.body);
-   res.redirect("/edit");
+   const task = req.body.task;
+   const num1 = taskList.length;
+
+   if(num1 !== 5 ){
+
+    if(task !== ""){
+        taskList.push(task);
+        taskLight = true;
+        res.redirect("/edit");
+       }else {
+        res.redirect("/edit"); 
+       }
+
+   }else{
+    console.log("MAX TASK")
+   }
+
+
+});
+
+app.post("/delete-task", function(req, res){
+    const task = req.body.task;
+    let result;
+    //console.log(task)
+       
+       for(let i = 0; i <taskList.length; i++){
+
+            if(taskList[i] === task){
+
+              taskList.splice(taskList.indexOf(task),1);
+              
+            }else{
+                // do nothing
+            }
+      
+       }
+
+       
+
+    console.log(taskList);
+
+    res.redirect("/edit");
 });
 
 
@@ -128,6 +187,7 @@ app.post("/add-task", function(req, res){
 app.listen(3000, function(req, res){
     console.log("Server started on port 3000!");
 });
+
 
 
 
@@ -146,4 +206,14 @@ app.listen(3000, function(req, res){
 
 //7# admin needs to be able to create notes per task
 
-//8# * create task function
+//8# admin should be able to delete already made task from data base.
+
+//9# create error messages "for max task created" and "task already exist"\
+
+
+
+
+
+
+
+
--- public/editPage.js
+++ public/editPage.js
@@ -3,8 +3,69 @@
 const saveBTN = document.querySelector("#save-btn");
 const backBTN = document.querySelector("#back-btn");
 const formBTN = document.querySelector("#form");
-const taskBTN = document.querySelector(".fa-plus");
 const inputValue = document.querySelector(".other-type-div");
+const taskLight = document.querySelector("#taskLight").value;
+const deleteTask = document.querySelector(".fa-trash-alt");
+const addTaskBTN = document.querySelector("#fa-plus");
+
+
+
+const task0 = document.querySelector("#task0");
+const task1 = document.querySelector("#task1");
+const task2 = document.querySelector("#task2");
+const task3 = document.querySelector("#task3");
+const task4 = document.querySelector("#task4");
+
+//form inputs
+const firstName = document.querySelector("#fName-i");
+const lastName = document.querySelector("#lName-i");
+const gender = document.querySelector("#gender-i");
+const address = document.querySelector("#address-i");
+const city = document.querySelector("#city-i");
+const state = document.querySelector("#state-i");
+const zip = document.querySelector("#zip-i");
+
+
+
+
+
+
+// sending total data to sever to save in database
+saveBTN.addEventListener("click", function(){
+    let url = "/client-bio";
+    
+    const ship = {
+        fname: firstName.value,
+        lname: lastName.value,
+        gender: gender.value,
+        address: address.value,
+        state: state.value,
+        city: city.value,
+        zip: zip.value
+     };
+     
+     fetch(url, {
+         method: "POST",
+         body: JSON.stringify(ship),
+         headers: {
+             'Accept': 'application/json',
+             'Content-Type': 'application/json'
+           }});
+           
+           // resetting value's to null
+           firstName.value = "";
+           lastName.value = "";
+           gender.value = "";
+           address.value = "";
+           city.value = "";
+           state.value ="";
+           zip.value = "";  
+  
+});
+
+// switches
+let taskSwitch = taskLight;
+
 
 
 // Event listnser
@@ -12,59 +73,96 @@ backBTN.addEventListener("click", function(){
    window.location = "/client";
 });
 
-
-
-taskBTN.addEventListener("click", function(){
-    const task = inputValue.value;
+// adding task to server ARRY
+addTaskBTN.addEventListener("click", function(){
+    let urlPath ="/add-task";
+    let selectedTask = inputValue.value;
     
+    sendTask(selectedTask, urlPath)
+    window.location.href ="/edit";
+});
 
-    if(task !== ""){
-    console.log(task);
-    inputValue.value = "";
 
-    }else{
 
-     // do nothing
+// section for deleting tasks btn
+task0.addEventListener("click", function(){
+    let selectedTask = task0.textContent;
+    let urlPath = "/delete-task";
+
+    sendTask(selectedTask, urlPath)
+    window.location.href ="/edit";
+});
+
+
+task1.addEventListener("click", function(){
+    let selectedTask = task1.textContent;
+    let urlPath = "/delete-task";
+
+    sendTask(selectedTask, urlPath)
+    window.location.href ="/edit";
+});
+
+
+task2.addEventListener("click", function(){
+    let selectedTask = task2.textContent;
+    let urlPath = "/delete-task";
+
+    sendTask(selectedTask, urlPath)
+    window.location.href ="/edit";
+});
+
+task3.addEventListener("click", function(){
+    let selectedTask = task3.textContent;
+    let urlPath = "/delete-task";
+       
+    sendTask(selectedTask, urlPath)
+    window.location.href ="/edit";
+});
+
+task4.addEventListener("click", function(){
+    let selectedTask = task4.textContent;
+    let urlPath = "/delete-task";
+
+    sendTask(selectedTask, urlPath)
+    window.location.href ="/edit";
+});
+
+
+
+
+  /// send selected task to server function
+function sendTask(task, url){
+
+    const ship = {
+       task: task,
+    };
+    
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify(ship),
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          }});
+    
+    }
+
+
+    function saveForm(url){
+
+        const ship = {
+            task: task,
+         };
+         
+         fetch(url, {
+             method: "POST",
+             body: JSON.stringify(ship),
+             headers: {
+                 'Accept': 'application/json',
+                 'Content-Type': 'application/json'
+               }});
 
     }
 
-});
 
-// let url = "/add-task";
-// let dictionary = task;
-
-// function post(dictionary, url, method) {
-// 	method = method || "post"; // post (set to default) or get
-
-// 	// Create the form object
-// 	var form = document.createElement("form");
-// 	form.setAttribute("method", method);
-// 	form.setAttribute("action", url);
-
-// 	// For each key-value pair
-// 	for (key in dictionary) {
-// 		//alert('key: ' + key + ', value:' + dictionary[key]); // debug
-// 		var hiddenField = document.createElement("input");	
-// 		hiddenField.setAttribute("type", "hidden"); 
-// 		hiddenField.setAttribute("name", key);
-// 		hiddenField.setAttribute("value", dictionary[key]);
-// 		// append the newly created control to the form
-// 		form.appendChild(hiddenField); 
-// 	}
-
-// 	document.body.appendChild(form); // inject the form object into the body section
-// 	form.submit();
-// }
-
-
-
-
-// let task = inputValue.value;
-// inputValue.value = "";
     
-//     var xhr = new XMLHttpRequest();
-//     xhr.open("POST", yourUrl, true);
-//     xhr.setRequestHeader('Content-Type', 'application/json');
-//     xhr.send(JSON.stringify({
-//         value: task
-//     }));
